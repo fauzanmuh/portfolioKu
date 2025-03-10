e916a90700c6ad331811208ec59e5548de2f5c0f
fauzanmuh <f4uz4n.oioi@gmail.com>
2025-03-10 20:29:53 +0700

    update

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 // typing text hero
 const typed = new Typed(".typing-text", {
-  strings: ["Web Developer"],
+  strings: ["Full Stack Web Developer"],
   loop: true,
   typeSpeed: 55,
   backSpeed: 25,
